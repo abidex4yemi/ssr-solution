65bb6b55f3a7d02e82071d8c633b2ce5e0a7d9ea
abidex4yemi <abidex4yemi@gmail.com>
2019-12-23 11:28:29 +0100

    Add:user  authentication routes

---
+++ api/routes/authRoutes.js
@@ -0,0 +1,33 @@
+const passport = require("passport");
+
+module.exports = app => {
+  app.get(
+    "/auth/google",
+    passport.authenticate("google", {
+      scope: ["profile", "email"]
+    })
+  );
+
+  app.get(
+    "/auth/google/callback",
+    passport.authenticate("google", (req, res) => {
+      return res.redirect("/");
+    })
+  );
+
+  app.get(
+    "/api/auth/google/callback",
+    passport.authenticate("google", (req, res) => {
+      return res.redirect("/");
+    })
+  );
+
+  app.get("/logout", (req, res) => {
+    req.logout();
+    return res.redirect("/");
+  });
+
+  app.get("/current_user", (req, res) => {
+    return res.send(req.user);
+  });
+};
